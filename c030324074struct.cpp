25dfb4a7be363f0eb748cc5c6b27dc35a4eff3b0
KurezzyK <jetsydumb197@gmail.com>
2025-05-27 14:25:14 +0800

    melakukan coding struct tanpa pointer

---
+++ c030324074struct.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+int main(){
+
+    mahasiswa hikmal;
+
+    hikmal.nim="c030324074";
+    hikmal.nama="M. Hikmal";
+    hikmal.alamat="Jl. Cempaka Raya Simp. Pinang Babaris";
+    hikmal.ipk=4;
+
+    cout << hikmal.nim<<"\n";
+    cout << hikmal.nama<<"\n";
+    cout << hikmal.alamat<<"\n";
+    cout << hikmal.ipk<<"\n";
+}
