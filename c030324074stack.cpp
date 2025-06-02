5c0381f168cdf83ecde29119646db9549ca49739
KurezzyK <jetsydumb197@gmail.com>
2025-06-03 00:32:12 +0800

    Menambahkan main dan function initialize stack

--- c030324074stack.cpp
+++ c030324074stack.cpp
@@ -10,3 +10,19 @@ typedef struct stack
     itemType Item[MAXSTACK];
     int Count;
 } Stack;
+
+void InitializeStack(Stack *S);
+
+int main(int argc, char const *argb[])
+{
+    Stack S;
+    itemType x;
+
+    InitializeStack(&S);
+
+    return 0;
+}
+
+void InitializeStack(Stack *S){
+    S->Count = 0;
+}
